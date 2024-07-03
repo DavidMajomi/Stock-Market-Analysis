22d3fdde7c7145909d352807e56a3d913adcf6cb
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-07-03 11:05:25 -0400

    Made running in IDE easier

--- Src/scripts/init_all_data.py
+++ Src/scripts/init_all_data.py
@@ -1,5 +1,9 @@
+import os
 from scrape import get_s_and_p_info
 from get_stock_data import get_S_and_p_ticker_and_listing_data_as_dataframe, get_price_data_and_populate_db
+
+# os.chdir("Src/scripts")
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def init_all_required_data() -> None:
         
