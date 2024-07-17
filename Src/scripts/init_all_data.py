6e557e5e0b4c7a96ebdfa600f4092b16e3e1ee38
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-07-17 17:34:23 -0400

    Update init_all_data.py

--- Src/scripts/init_all_data.py
+++ Src/scripts/init_all_data.py
@@ -4,9 +4,6 @@ from scrape import get_s_and_p_info, get_most_active_stocks_from_yfinance
 from get_stock_data import get_S_and_p_ticker_and_listing_data_as_dataframe, get_price_data_and_populate_db, populate_db_with_most_active_stocks
 from get_stock_news import get_and_store_all_s_and_p_news_from_yfinance_in_DB, get_and_store_news_api_org_todays_top_business_headlines
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 # os.chdir("Src/scripts")
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
