ac96f5447eb3aff1764150a67c1295375f526279
jaydentani <72216056+jaydentani@users.noreply.github.com>
2024-07-18 11:45:57 -0700

    Create create_lstm_df.py

---
+++ Price Prediciton/create_lstm_df.py
@@ -0,0 +1,44 @@
+days_back = 4
+
+def get_range(df, start, end):
+    start = pd.to_datetime(start)
+    end = pd.to_datetime(end)
+
+    df = df.loc[start:end]
+    
+    # Univariate but will change to multivariate
+    df = df[["Close"]]
+
+    # Creates new columns
+    for x in range(1, n + 1):            
+        df[f"target - {x}"] = df["Close"].shift(x)
+
+    
+    
+    
+    # df["target - 1"] = df["Close"].shift(1)
+    # df["target - 2"] = df["Close"].shift(2)
+    # df["target - 3"] = df["Close"].shift(3)
+    
+    
+    
+    df.dropna(inplace = True)
+     
+    return df
+
+
+def seperate_df(df):
+    dates = df.index  
+    X = df.iloc[:, 1:n + 1]
+    Y = df["Close"]
+
+    return dates, X, Y
+
+df = get_range(map["HD"], "1-1-2023", "12-31-2024")
+
+dates, X, y = seperate_df(df)
+
+
+X = X.to_numpy()
+X = X.reshape(len(dates), X.shape[1], 1)
+    
