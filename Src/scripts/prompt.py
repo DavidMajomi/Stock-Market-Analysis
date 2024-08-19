7b29c2302e11da9de263db0c1ab160218f1348d2
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-08-18 23:32:57 -0400

    Create prompt.py

---
+++ Src/scripts/prompt.py
@@ -0,0 +1,34 @@
+import google.generativeai as genai
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def write_api_key_to_file(key, file):
+    f = open(file, "w")
+    f.write(key)
+    f.close()
+
+
+def read_api_key_from_file(path_to_file : str) -> str:
+    if os.path.exists(path_to_file):
+        with open(path_to_file, "r") as file:
+            api_key = file.readline()
+    else:
+        raise Exception("File with api key does not exist")
+    
+    return api_key 
+
+
+
+
+def prompt(message):
+    genai.configure(api_key=read_api_key_from_file("../API keys/gemini_api_key.txt"))
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    
+    intstruction = "" + message
+
+    response = model.generate_content(intstruction)
+    
+    return response.text
+
