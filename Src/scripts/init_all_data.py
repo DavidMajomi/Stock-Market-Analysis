c377ffe1c7aa9746c0563101209fb39d6504a4e4
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2025-03-01 19:14:59 -0500

    Update init_all_data.py

--- Src/scripts/init_all_data.py
+++ Src/scripts/init_all_data.py
@@ -30,26 +30,29 @@ def init_all_required_data() -> None:
     
     s_and_p_requirements = threading.Thread(target=get_s_and_p_data)
 
-    yf_most_active = threading.Thread(target=get_most_active)
+    # yf_most_active = threading.Thread(target=get_most_active)
 
     news_api_headlines = threading.Thread(target=get_and_store_news_api_org_todays_top_business_headlines)
     
     
     s_and_p_requirements.start()
-    yf_most_active.start()
+    # yf_most_active.start()
     news_api_headlines.start()
     
     
     news_api_headlines.join()
     print("Done getting top business headlines from newsapi.org")
     
-    yf_most_active.join()
+    # yf_most_active.join()
     s_and_p_requirements.join()
     
-    get_and_store_all_s_and_p_news_from_yfinance_in_DB()
-    print("Done getting all s and p news from yfinance")
+    # get_and_store_all_s_and_p_news_from_yfinance_in_DB()
+    # print("Done getting all s and p news from yfinance")
     
 
     
     
         
+        
+if __name__ == "__main__":
+    get_s_and_p_data()
