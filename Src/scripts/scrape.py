90ec639e52386e7d490bd20683b5a12d7342fc86
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2025-03-14 19:00:13 -0400

    Update scraping

--- Src/scripts/scrape.py
+++ Src/scripts/scrape.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import path_constants
+from io import StringIO
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,41 +23,65 @@ def get_s_and_p_info():
     
 
 def get_most_active_stocks_from_yfinance():
-    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
     
-    url='https://finance.yahoo.com/most-active/'
-    response=requests.get(url,headers=headers)
-
-    soup=BeautifulSoup(response.content,'lxml')
-    
-    most_active_stocks_by_order = []
-    
-    for item in soup.select('.simpTblRow'):
-        symbol = item.select('[aria-label=Symbol]')[0].get_text()
-        Name = item.select('[aria-label=Name]')[0].get_text()
-        price = item.select('[aria-label*=Price]')[0].get_text()
-        change = item.select('[aria-label=Change]')[0].get_text()
-        percent_change = item.select('[aria-label="% Change"]')[0].get_text()
-        Volume = item.select('[aria-label="Volume"]')[0].get_text()
-        avg_vol_3_month = item.select('[aria-label="Avg Vol (3 month)"]')[0].get_text()
-        mkt_cap = item.select('[aria-label="Market Cap"]')[0].get_text()
-        pe_ratio = item.select('[aria-label="PE Ratio (TTM)"]')[0].get_text()
-
-        ticker = {
-            "symbol" : symbol,
-            "Name" : Name,
-            "price" : price,
-            "change" : change,
-            "percent_change" : percent_change,
-            "Volume" : Volume,
-            "avg_vol_3_month" : avg_vol_3_month,
-            "mkt_cap" : mkt_cap,
-            "pe_ratio" : pe_ratio
-        }
+    try:
         
-        most_active_stocks_by_order.append(ticker)
+        headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
         
-        df = pd.DataFrame(most_active_stocks_by_order)
+        url='https://finance.yahoo.com/most-active/'
+        response=requests.get(url,headers=headers)
+
+        
+        html = response.text
+        
+        dataframes = pd.read_html(StringIO(html))
+        df = dataframes[0]
+        
+        df.drop(columns=["52 Wk Range", "Unnamed: 2"], inplace=True)
+        
+        
+        price_list = df["Price"].to_list()
+
+        for i in range(len(price_list)):
+            temp = price_list[i]
+            first_space_in_str = temp.find(' ')
+            price_list[i] = temp[0:first_space_in_str]
+            
+            
+        Price = pd.Series(price_list)
+        # print(Price)
+
+        # df.replace({'Price': Price})
+        df["Price"] = Price
+        
+        df.rename(columns={
+            'Symbol': 'symbol', 
+            'Change %': 'percent_change', 
+            'Avg Vol (3M)': 'avg_vol_3_month', 
+            "Price" : "price",
+            "Change" : "change",
+            "Market Cap": "mkt_cap", 
+            "P/E Ratio (TTM)" : "pe_ratio", 
+            "52 Wk Change %" : "52_wk_percent_change"
+            }, inplace=True)
+        
         df.to_csv(PATH_TO_CSV_WITH_MOST_ACTIVE_STOCKS, index=False)
         
+    except requests.exceptions.Timeout:
+        print("The request timed out.")
+    except requests.exceptions.HTTPError as errh:
+        print(f"HTTP error occurred: {errh}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+    except PermissionError:
+        print("Permission error. Please check the file path.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+    
+if __name__ == '__main__':
+    # get_yfinace_using_pandas_scraping()
+    get_most_active_stocks_from_yfinance()
+    
     
